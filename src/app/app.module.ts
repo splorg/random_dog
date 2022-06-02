6cd4fe0a520272d8047aa2a35fae6ae38ba14b24
gabriel.lima <gabriel.limabs02@gmail.com>
2022-06-02 09:59:35 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DogComponent } from './dog/dog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
